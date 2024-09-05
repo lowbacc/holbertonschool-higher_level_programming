5e63dd00dce5c26e8305f9a8a3a978f9217e465e
lowbacc <skalski.matthis@gmail.com>
2024-09-05 12:05:24 +0200

    other code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+    if i < 99:
+        print(f"{i:02d}, ", end="")
+    else:
+        print(f"{i:02d}")
