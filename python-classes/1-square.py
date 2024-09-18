77480c9782b042f806ba5a04a49161fdde782c8f
lowbacc <skalski.matthis@gmail.com>
2024-09-18 22:10:43 +0200

    code for the second task, square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that defines a square."""
+
+
+class Square:
+    """A class to represent a square"""
+
+    def __init__(self, size=0):
+        """Initializes a Square instance."""
+        self.__size = size
