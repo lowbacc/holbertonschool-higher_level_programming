c84c57a7b50dd2690ba527a5a92a06c22a1ec9ff
lowbacc <skalski.matthis@gmail.com>
2024-09-24 13:26:37 +0200

    correction for the second and the third tasks, blank line missed and interpreter was forgot

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A class MyList which inherits the list"""
 
+
 class MyList(list):
     """A subclass of the built-in list class."""
 
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""function that returns True if the object is exactly
+an instance of the specified class"""
+
+
 def is_same_class(obj, a_class):
     """
     Check if obj is exactly an instance of a_class.
