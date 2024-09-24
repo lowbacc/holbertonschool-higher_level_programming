0ed7e4cea901be68447e5c2b285371a4308b20a5
lowbacc <skalski.matthis@gmail.com>
2024-09-24 12:49:13 +0200

    code for the first task, lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list of names of the object's attributes and methods.
+    """
+    return dir(obj)
