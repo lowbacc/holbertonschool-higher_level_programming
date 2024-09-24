7d113468b46d723077c9f38225cedf621d2cafc8
lowbacc <skalski.matthis@gmail.com>
2024-09-24 13:29:50 +0200

    correction for the code of the tasks 4 and 5, interpreter was forgot and pycodestyle error

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+returns True if the object is an instance of, or if the object is an instance
+of a class that inherited from, the specified class """
+
+
 def is_kind_of_class(obj, a_class):
     """
     Check if obj is an instance of or inherits from a_class.
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,23 @@
+#!/usr/bin/python3
+"""
+defines a function that looks if the object is an instance of the class
+directly or indirectly
+"""
+
+
 def inherits_from(obj, a_class):
     """
-    Check if obj is an instance of a subclass of a_class.
+    Returns True if the object is an instance of,
+    or if the object is an instance of a class
+    that inherited from, the specified class (directly or indirectly);
+    otherwise False.
 
     Args:
         obj: The object to check.
-        a_class: The class to compare against.
+        a_class: The class to check against.
 
     Returns:
-        True if obj is an instance of a subclass of a_class, False otherwise.
+        bool: True if obj is an instance or inherited instance of a_class,
+        False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
