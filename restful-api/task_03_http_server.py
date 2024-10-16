65ad159154369128558de0b6c6caf98c71df80b8
lowbacc <skalski.matthis@gmail.com>
2024-10-16 15:16:18 +0200

    Add some documentation on task 4, http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,9 +2,11 @@ import http.server
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# define the handler class
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        # set the response status code
         if self.path == '/':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -38,11 +40,13 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             error_message = {"error": "Endpoint not found"}
             self.wfile.write(json.dumps(error_message).encode())
 
+# define the run function
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting http server on port {port}")
     httpd.serve_forever()
 
+# run the server
 if __name__ == "__main__":
     run()
