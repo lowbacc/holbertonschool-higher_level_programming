66b6d636aeb006eadbb940758f6fb5289d67c06d
lowbacc <skalski.matthis@gmail.com>
2024-10-16 15:33:49 +0200

    code correction for the task 4, http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,8 +31,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode())
+            self.wfile.write(b"Endpoint not found")
 
 '''define the run function'''
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
