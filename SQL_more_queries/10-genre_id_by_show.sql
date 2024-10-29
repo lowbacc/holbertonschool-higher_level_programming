f482207bc35409ceb0251935ba5e2c3b1182bf95
lowbacc <skalski.matthis@gmail.com>
2024-10-29 23:34:52 +0100

    Add SQL query to order TV shows by title and genre_id

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Order the results by the title of the TV show in ascending order and the genre_id in ascending order.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
