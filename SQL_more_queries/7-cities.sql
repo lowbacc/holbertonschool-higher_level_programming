68801b072474274817fd95d31d1712022dc089ea
lowbacc <skalski.matthis@gmail.com>
2024-10-29 23:31:07 +0100

    i reworked on this code for more accuracy

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,17 @@
 -- Create a new table cities with the following fields:
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbtn_0d_usa')
+BEGIN
+    CREATE DATABASE hbtn_0d_usa;
+END;
 
 USE hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'cities')
+BEGIN
+    CREATE TABLE cities (
+    id INT IDENTITY(1,1) UNIQUE NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
-);
+    );
+END;
