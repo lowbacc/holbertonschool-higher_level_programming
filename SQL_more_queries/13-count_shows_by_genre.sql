470058f41efa651ea0e38862e5a79f9cf7884487
lowbacc <skalski.matthis@gmail.com>
2024-10-29 23:55:02 +0100

    reworking of the code

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,14 @@
--- Count the number of shows for each genre. Order the result by the number of shows in descending order.
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
+-- Count the number of shows for each genre.
+SELECT 
+    g.name AS genre, 
+    COUNT(sg.show_id) AS number_of_shows
+FROM 
+    genres AS g
+JOIN 
+    shows_genres AS sg ON g.id = sg.genre_id
+GROUP BY 
+    g.name
+HAVING 
+    COUNT(sg.show_id) > 0
+ORDER BY 
+    number_of_shows DESC;
