3df55601f15142427c715f1881b48b410885fcbb
lowbacc <skalski.matthis@gmail.com>
2024-10-30 00:11:29 +0100

    Remove user creation and privilege grant statements; update comments for clarity

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,20 +1,3 @@
--- Create user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to ensure that all changes take effect
-FLUSH PRIVILEGES;
-
--- Show grants for user_0d_1
+-- Show privileges for the users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
