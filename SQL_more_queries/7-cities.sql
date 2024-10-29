120a5041e16f4afe1cc271d0b1f04068ef47a33a
lowbacc <skalski.matthis@gmail.com>
2024-10-29 23:24:28 +0100

    Add SQL script to create table cities with foreign key reference to states

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create a new table cities with the following fields:
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
