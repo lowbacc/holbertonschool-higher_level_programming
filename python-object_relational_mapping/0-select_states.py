9210ff484e9e94673b7d36288d452afe4160bd1c
lowbacc <skalski.matthis@gmail.com>
2024-10-31 09:37:58 +0100

    Add docstrings to 0-select_states.py and clean up output loop

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
+'''lists all states from the database hbtn_0e_0_usa'''
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command-line arguments
+    '''main method'''
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
@@ -25,9 +28,7 @@ if __name__ == "__main__":
 
     # Fetch all rows and print them in the desired format
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
+    
     # Close cursor and connection
     cur.close()
     db.close()
