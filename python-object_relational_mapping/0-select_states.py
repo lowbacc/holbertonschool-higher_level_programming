6861bca2caed5c9df63ab141cc65e6c789a9c635
lowbacc <skalski.matthis@gmail.com>
2024-10-31 10:20:48 +0100

    Fix MySQL connection by moving port parameter to the correct position

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        port=3306
     )
 
     # Create a cursor to execute SQL queries
